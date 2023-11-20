f4cecb636602fbb5f2cce1bce619175fbb507da4
EhanAlejandroM <leehoseok905@gmail.com>
2023-11-20 17:29:27 -0500

    Se agregó la seccion de reseñas y se le agrego estilos y funcionalidad

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 60)
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var swiper = new Swiper('.review-slider', {
+        slidesPerView: 'auto',
+        spaceBetween: 30,
+        mousewheel: true,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+});
